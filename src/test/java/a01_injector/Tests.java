4acf5ccf27a1dcce82e5fcca6d6266be55fb3372
Krzysztof Kot <krzykot@gmail.com>
2017-12-17 17:52:13 +0100

    Initial

---
+++ src/test/java/a01_injector/Tests.java
@@ -0,0 +1,76 @@
+package a01_injector;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import org.junit.Test;
+
+import javax.inject.Singleton;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+
+interface IEditor {
+
+}
+
+class Editor implements IEditor {
+    Editor() {
+        System.out.println("Creating editor");
+    }
+
+}
+
+class Editor2 implements IEditor {
+    Editor2() {
+        System.out.println("Creating editor2");
+    }
+}
+
+
+public class Tests {
+
+    @Test
+    public void shouldReturnInstance_whenNoModuleAndRetrievedByClass() {
+        Injector injector = Guice.createInjector();
+
+        // no module or explicit binding is defined so JIT binding is used https://github.com/google/guice/wiki/JustInTimeBindings
+        Editor editor = injector.getInstance(Editor.class);
+        assertThat(editor, not(nullValue()));
+    }
+
+    @Test
+    public void shouldReturnInstanceOfRightClass_whenModuleIsSetAndRetrievedByInterface() {
+        Module module = binder -> binder.bind(IEditor.class).to(Editor2.class);
+        Injector injector = Guice.createInjector(module);
+
+        IEditor editor = injector.getInstance(IEditor.class);
+        assertThat(editor, instanceOf(Editor2.class));
+    }
+
+    @Test
+    public void shouldReturnNewInstance_whenScopeNotDefined() {
+        Injector injector = Guice.createInjector();
+        Editor editor1 = injector.getInstance(Editor.class);
+        Editor editor2 = injector.getInstance(Editor.class);
+        assertThat(editor1, not(equalTo(editor2)));
+    }
+
+    @Test
+    public void shouldReturnSameInstance_whenScopeSingleton() {
+        Injector injector = Guice.createInjector(binder ->
+                binder.bind(Editor.class).in(Singleton.class)
+        );
+        Editor editor1 = injector.getInstance(Editor.class);
+        Editor editor2 = injector.getInstance(Editor.class);
+        assertThat(editor1, equalTo(editor2));
+
+        // but we still can create new instance by new or other injector
+        Injector injector2 = Guice.createInjector();
+        assertThat(editor1, not(equalTo(injector2.getInstance(Editor.class))));
+
+        Editor editor = new Editor(); // constructor should not be public https://github.com/google/guice/wiki/KeepConstructorsHidden
+        assertThat(editor1, not(equalTo(editor)));
+    }
+
+}
---
+++ src/test/java/basic02/Basic02_InjectingDependencies.java
@@ -0,0 +1,183 @@
+package basic02;
+
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.name.Names;
+import org.junit.Test;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+interface IDependencyService {
+
+}
+
+class DependencyService implements IDependencyService {
+
+}
+
+class CachedDependencyService implements IDependencyService {
+
+}
+
+public class Basic02_InjectingDependencies {
+
+    static class EditorField {
+        @Inject
+        private IDependencyService dependencyService;
+
+        public IDependencyService getDependencyService() {
+            return dependencyService;
+        }
+    }
+
+    @Test
+    public void shouldInjectService_whenFieldIsAnnotated() {
+        // Given
+        Injector injector = Guice.createInjector(binder -> {
+            binder.bind(IDependencyService.class).to(DependencyService.class);
+        });
+
+        // When
+        EditorField editor = injector.getInstance(EditorField.class);
+
+        // Then
+        assertThat(editor.getDependencyService(), notNullValue());
+    }
+
+    static class EditorSet {
+
+        private IDependencyService dependencyService;
+
+        @Inject
+        void setDependencyService(IDependencyService dependencyService) {
+            this.dependencyService = dependencyService;
+        }
+
+        public IDependencyService getDependencyService() {
+            return dependencyService;
+        }
+    }
+
+    @Test
+    public void shouldInjectService_whenSetIsAnnotated() {
+        // Given
+        Injector injector = Guice.createInjector(binder -> {
+            binder.bind(IDependencyService.class).to(DependencyService.class);
+        });
+
+        // When
+        EditorSet editor = injector.getInstance(EditorSet.class);
+
+        // Then
+        assertThat(editor.getDependencyService(), notNullValue());
+    }
+
+    static class EditorConstructor {
+
+        private IDependencyService dependencyService;
+
+        @Inject
+        public EditorConstructor(IDependencyService dependencyService) {
+            this.dependencyService = dependencyService;
+        }
+
+        public IDependencyService getDependencyService() {
+            return dependencyService;
+        }
+    }
+
+    @Test
+    public void shouldInjectService_whenConstructorIsAnnotated() {
+        // Given
+        Injector injector = Guice.createInjector(binder -> {
+            binder.bind(IDependencyService.class).to(DependencyService.class);
+        });
+
+        // When
+        EditorConstructor editor = injector.getInstance(EditorConstructor.class);
+
+        // Then
+        assertThat(editor.getDependencyService(), notNullValue());
+    }
+
+    static class EditorBothServices {
+
+        private IDependencyService dependencyService;
+        private IDependencyService cachedDependencyService;
+
+        @Inject
+        public EditorBothServices(IDependencyService dependencyService,
+                                  @Named("Cached") IDependencyService cachedDependencyService) {
+            this.dependencyService = dependencyService;
+            this.cachedDependencyService = cachedDependencyService;
+        }
+
+        public IDependencyService getDependencyService() {
+            return dependencyService;
+        }
+
+        public IDependencyService getCachedDependencyService() {
+            return cachedDependencyService;
+        }
+    }
+
+    @Test
+    public void shouldInjectNamed() {
+        // Given
+        Injector injector = Guice.createInjector(binder -> {
+            binder.bind(IDependencyService.class).to(DependencyService.class);
+            binder.bind(IDependencyService.class).annotatedWith(Names.named("Cached")).to(CachedDependencyService.class);
+        });
+
+        // When
+        EditorBothServices editor = injector.getInstance(EditorBothServices.class);
+
+        // Then
+        assertThat(editor.getDependencyService(), instanceOf(DependencyService.class));
+        assertThat(editor.getCachedDependencyService(), instanceOf(CachedDependencyService.class));
+    }
+
+    static class EditorBothServicesAnnotation {
+
+        private IDependencyService dependencyService;
+        private IDependencyService cachedDependencyService;
+
+        @Inject
+        public EditorBothServicesAnnotation(IDependencyService dependencyService,
+                                  @Cached IDependencyService cachedDependencyService) {
+            this.dependencyService = dependencyService;
+            this.cachedDependencyService = cachedDependencyService;
+        }
+
+        public IDependencyService getDependencyService() {
+            return dependencyService;
+        }
+
+        public IDependencyService getCachedDependencyService() {
+            return cachedDependencyService;
+        }
+    }
+
+    @Test
+    public void shouldInjectAnnotated() {
+        // Given
+        Injector injector = Guice.createInjector(binder -> {
+            binder.bind(IDependencyService.class).to(DependencyService.class);
+            binder.bind(IDependencyService.class).annotatedWith(Cached.class).to(CachedDependencyService.class);
+        });
+
+        // When
+        EditorBothServicesAnnotation editor = injector.getInstance(EditorBothServicesAnnotation.class);
+
+        // Then
+        assertThat(editor.getDependencyService(), instanceOf(DependencyService.class));
+        assertThat(editor.getCachedDependencyService(), instanceOf(CachedDependencyService.class));
+    }
+
+}
